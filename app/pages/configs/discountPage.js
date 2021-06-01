03097b0ffd33f8202caeb9c00f9cbf97abb02f24
Marina <m.trojchenko@gmail.com>
2021-06-01 12:03:36 +0300

    all IM

---
+++ app/pages/configs/discountPage.js
@@ -0,0 +1,28 @@
+const config = {
+    file: 'index.html',
+    placeholders: {
+        header: {
+            copies: 1,
+            file: 'header\\headerIM.html',
+            placeholders: {}
+        },
+        menu: {
+            copies: 1,
+            file: 'menu\\menuIM.html',
+            placeholders: {}
+        },
+        pagesContent: {
+            file: 'discountPage\\discountPage.html',
+            placeholders: {}
+        },
+        footer: {
+            file: 'footer\\footer.html',
+            placeholders: {}
+        },
+        popup: {
+            file: 'popup\\popup.html',
+            placeholders: {}
+        },
+    }
+};
+module.exports = config;
